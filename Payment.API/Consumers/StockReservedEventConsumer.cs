c238bc5aee3a544f12195208eb14e9cbd0e23de5
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-03-06 18:19:24 +0300

    PaymentAPI ' de consumer ımızı oluşturduk

---
+++ Payment.API/Consumers/StockReservedEventConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace Payment.API.Consumers
+{
+    public class StockReservedEventConsumer() : IConsumer<StockReservedEvent>
+    {
+        public Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            if (true)
+            {
+                //Ödeme Başarılı...
+            }
+            else
+            {
+                //Ödeme Başarısız...
+            }
+        }
+    }
+}
