4b8f4c685951c869959d9235a9328396051874ab
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-03-07 20:16:29 +0300

    orderstatemachine işlemleri

--- SagaStateMachine.Service/Program.cs
+++ SagaStateMachine.Service/Program.cs
@@ -1,9 +1,21 @@
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachine.Service.StateDbContext;
+using SagaStateMachine.Service.StateInstances;
+using SagaStateMachine.Service.StateMachines;
 
 var builder = Host.CreateApplicationBuilder(args);
 //builder.Services.AddHostedService<Worker>();
 builder.Services.AddMassTransit(configurator =>
 {
+    configurator.AddSagaStateMachine<OrderStateMachine,OrderStateInstance>()
+    .EntityFrameworkRepository(opt =>
+    {
+        opt.AddDbContext<DbContext, OrderStateDbContext>((provider, _builder) =>
+        {
+            _builder.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
+        })
+    });
     configurator.UsingRabbitMq((context, _configure) =>
     {
         _configure.Host(builder.Configuration["RabbitMQ"]);
---
+++ SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
@@ -0,0 +1,12 @@
+﻿using MassTransit;
+using SagaStateMachine.Service.StateInstances;
+
+namespace SagaStateMachine.Service.StateMachines;
+
+public class OrderStateMachine:MassTransitStateMachine<OrderStateInstance>
+{
+    public OrderStateMachine()
+    {
+        InstanceState(instance => instance.CurrentState);
+    }
+}
