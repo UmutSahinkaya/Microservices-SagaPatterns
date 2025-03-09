cff26091dfd565cb7752c2e6a85df750dfc12c25
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2025-03-09 11:51:46 +0300

    Order ve Stock daki gerekli Consumerların yazımı

---
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared;
+using Shared.OrderEvents;
+using Shared.StockEvents;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers;
+
+public class OrderCreatedEventConsumer(MongoDBService mongoDBService,ISendEndpointProvider sendEndpointProvider) : IConsumer<OrderCreatedEvent>
+{
+    public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+    {
+        List<bool> stockResults = new();
+        var stockCollection = mongoDBService.GetCollection<Models.Stock>();
+
+        foreach (var orderItem in context.Message.OrderItems)
+            stockResults.Add(await (await stockCollection.FindAsync(x => x.ProductId == orderItem.ProductId && x.Count >= orderItem.Count)).AnyAsync());
+        var sendEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.StateMachineQueue}"));
+
+        if (stockResults.TrueForAll(x => x.Equals(true)))
+        {
+            foreach(var orderItem in context.Message.OrderItems)
+            {
+                var stock = await(await stockCollection.FindAsync(x => x.ProductId == orderItem.ProductId)).FirstOrDefaultAsync();
+                stock.Count -= orderItem.Count;
+
+                await stockCollection.FindOneAndReplaceAsync(x => x.ProductId == orderItem.ProductId, stock);
+            }
+            StockReservedEvent stockReservedEvent = new(context.Message.CorrelationId)
+            {
+                OrderItems = context.Message.OrderItems
+            };
+
+            await sendEndpoint.Send(stockReservedEvent);
+        }
+        else
+        {
+            StockNotReservedEvent stockNotReservedEvent = new(context.Message.CorrelationId)
+            {
+                Message = "Stock yetersiz."
+            };
+            await sendEndpoint.Send(stockNotReservedEvent);
+        }
+    }
+}
---
+++ Stock.API/Consumers/StockRollbackMessageConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Shared.Messages;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers
+{
+    public class StockRollbackMessageConsumer(MongoDBService mongoDBService) : IConsumer<StockRollbackMessage>
+    {
+        public Task Consume(ConsumeContext<StockRollbackMessage> context)
+        {
+            var stockCollection= mongoDBService.GetCollection<Models.Stock>();
+
+        }
+    }
+}
